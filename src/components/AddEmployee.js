82eef7b91e25aea3472de6954d284fae756f01d3
Antonio <ajenkins2@student.fullsail.edu>
2022-09-17 14:29:38 -0700

    ADDED: EmployeeServices to connect to the backend

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
+    const [employee, setEmployee] = useState({
+        firstName: '',
+        lastName: '',
+        emailId: '',
+    });
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const value = e.target.value;
+        setEmployee({
+            ...employee,
+            [e.target.name]: value,
+        });
+    };
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        EmployeeService.saveEmployee(employee)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <div className="flex max-w-2xl mx-auto shadow border-b">
             <div className="px-8 py-8">
@@ -14,6 +41,9 @@ const AddEmployee = () => {
                     <input
                         className="h-10 w-96 border mt-2 px-2 py-2"
                         type="text"
+                        name="firstName"
+                        value={employee.firstName}
+                        onChange={(e) => handleChange(e)}
                     ></input>
                 </div>
                 <div className="item-center justify-center h-14 w-full my-4">
@@ -23,6 +53,9 @@ const AddEmployee = () => {
                     <input
                         className="h-10 w-96 border mt-2 px-2 py-2"
                         type="text"
+                        name="lastName"
+                        value={employee.lastName}
+                        onChange={(e) => handleChange(e)}
                     ></input>
                 </div>
                 <div className="item-center justify-center h-14 w-full my-4">
@@ -32,10 +65,16 @@ const AddEmployee = () => {
                     <input
                         className="h-10 w-96 border mt-2 px-2 py-2"
                         type="email"
+                        name="emailId"
+                        value={employee.emailId}
+                        onChange={(e) => handleChange(e)}
                     ></input>
                 </div>
                 <div className="item-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-                    <button className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-500">
+                    <button
+                        onClick={saveEmployee}
+                        className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-500"
+                    >
                         Save
                     </button>
                     <button className="rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-500">
