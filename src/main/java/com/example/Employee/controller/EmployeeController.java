7cd098ede7b98cb79d11c2f4beda39be813ac3ef
Antonio <ajenkins2@student.fullsail.edu>
2022-09-19 20:43:25 -0700

    CORRECTED: updateEmployee method wasn't set to the right variable

--- src/main/java/com/example/Employee/controller/EmployeeController.java
+++ src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.Employee.controller;
 
 import com.example.Employee.modal.Employee;
 import com.example.Employee.services.EmployeeService;
+import org.apache.coyote.Response;
 import org.apache.el.parser.BooleanNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -42,5 +43,18 @@ public class EmployeeController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", deleted);
         return ResponseEntity.ok(response);
+    }
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeId(@PathVariable Long id){
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeId(@PathVariable Long id,
+                                                  @RequestBody Employee employee) {
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
     }
 }
